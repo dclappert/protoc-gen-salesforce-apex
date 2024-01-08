cb422e75ead474b69e39953d8332e70101fd658f
David Lappert <dclappert@gmail.com>
2024-01-08 02:28:25 +0000

    test: adds integration tests to assert apex and metadata files are generated successful
    Adds integration tests to ensure that Apex Classes and associated metadata files are generated
    successfully and match expected output

--- scripts/compile-and-run.sh
+++ scripts/compile-and-run.sh
@@ -5,5 +5,5 @@ go install github.com/dclappert/protoc-gen-salesforce-apex/cmd/protoc-gen-salesf
   && protoc \
     --proto_path=./examples/proto \
     --salesforce-apex_out=./examples/target/classes \
-    --salesforce-apex_opt=apiVersion="57.0",useProtoFieldNames=true \
-    example.proto
+    --salesforce-apex_opt=apiVersion="56.0",useProtoFieldNames=false \
+    explorer_note.proto adventure_log.proto
